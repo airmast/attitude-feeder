424d1584167f7e5350f9b14f160e6255b41020ab
Mikhail Krutyakov <mkrutyakov@ugcs.com>
2017-09-20 11:34:06 +0300

    Fix snapcraft config

--- snap/plugins/x_qtdependencies.py
+++
@@ -1,7 +0,0 @@
-import snapcraft
-
-
-class QtdependenciesPlugin(snapcraft.BasePlugin):
-    def __init__(self, name, options, project):
-        super().__init__(name, options, project)
-        self.build_packages.extend(['libqt5serialport5-dev', 'libqt5network5', 'qtscript5-dev'])
